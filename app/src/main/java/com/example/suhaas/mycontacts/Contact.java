ecac57f01d9ab720d1de6cc0368d4a5ffdd40e18
= <=>
2015-11-04 13:15:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/suhaas/mycontacts/Contact.java
@@ -0,0 +1,16 @@
+package com.example.suhaas.mycontacts;
+
+/**
+ * Created by ravindrakumar on 03/11/15.
+ */
+public class Contact {
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    private String mName;
+}
---
+++ app/src/main/java/com/example/suhaas/mycontacts/ContactListActivity.java
@@ -0,0 +1,73 @@
+package com.example.suhaas.mycontacts;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ContactListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+
+
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+        Contact contact1 = new Contact();
+        contact1.setName("Suhaas.S");
+        contacts.add(contact1);
+        ListView listView = (ListView) findViewById(R.id.contact_list_view);
+        listView.setAdapter(new ContactAdapter(contacts));
+
+    }
+
+    private class ContactAdapter extends ArrayAdapter<Contact>{
+        ContactAdapter(ArrayList<Contact> contacts){
+            super(ContactListActivity.this, R.layout.contact_list_row, R.id.contact_row_name, contacts);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = super.getView(position, convertView, parent);
+
+            Contact contact = getItem(position);
+
+            TextView nameTextView = (TextView) convertView.findViewById(R.id.contact_row_name);
+            nameTextView.setText(contact.getName());
+            return convertView;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_contact_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

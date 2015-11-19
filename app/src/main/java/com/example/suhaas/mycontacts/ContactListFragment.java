4ecbc7519e06e4a9e3a87f9be94f3994e06dc7a7
= <=>
2015-11-19 20:40:52 +0530

    Add fragment

---
+++ app/src/main/java/com/example/suhaas/mycontacts/ContactListFragment.java
@@ -0,0 +1,162 @@
+    package com.example.suhaas.mycontacts;
+
+
+    import android.app.Activity;
+    import android.app.Fragment;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.support.v7.app.ActionBar;
+    import android.support.v7.app.ActionBarActivity;
+    import android.support.v7.app.AppCompatActivity;
+    import android.view.LayoutInflater;
+    import android.view.Menu;
+    import android.view.MenuInflater;
+    import android.view.MenuItem;
+    import android.view.View;
+    import android.view.ViewGroup;
+    import android.widget.AbsListView;
+    import android.widget.AdapterView;
+    import android.widget.ArrayAdapter;
+    import android.widget.ListView;
+    import android.widget.TextView;
+
+    import java.util.ArrayList;
+
+
+    /**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContactListFragment extends Fragment {
+
+        private ContactList mContacts;
+        private ContactAdapter mAdapter;
+        private Contract mContract;
+
+
+    public ContactListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =inflater.inflate(R.layout.fragment_contact_list, container, false);
+
+        mContacts = ContactList.getInstance();
+        for (int i = 0; i <30 ; i++) {
+            Contact contact1 = new Contact();
+            contact1.setName("Suhaas.S");
+            contact1.emails =new ArrayList<String>();
+            contact1.emails.add("suhaas.sreedhar@gmail.com");
+            contact1.emails.add("suhaas.s.minu@gmail.com");
+            contact1.phoneNumbers =new ArrayList<String>();
+            contact1.phoneNumbers.add("1800000005");
+            contact1.phoneNumbers.add("1800000000");
+            mContacts.add(contact1);
+        }
+
+
+        ListView listView = (ListView)v.findViewById(R.id.contact_list_view);
+        mAdapter = new ContactAdapter(mContacts);
+        listView.setAdapter(mAdapter);
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            int previousFirstItem = 0;
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                ActionBar ab = ((AppCompatActivity) getActivity()).getSupportActionBar();
+                if (firstVisibleItem > previousFirstItem) {
+                    ab.hide();
+                } else if (firstVisibleItem < previousFirstItem) {
+                    ab.show();
+                }
+
+                previousFirstItem = firstVisibleItem;
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (mContract != null) {
+                    mContract.selectedPosition(position);
+                }
+            }
+        });
+        return v;
+    }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            mAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            try {
+                mContract = (Contract) getActivity();
+            }catch (ClassCastException e){
+                throw new IllegalStateException("Activity does not implement contract");
+            }
+        }
+
+        @Override
+        public void onDetach() {
+            super.onDetach();
+            mContract = null;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            super.onCreateOptionsMenu(menu, inflater);
+            inflater.inflate(R.menu.menu_contact_list, menu);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+
+        private class ContactAdapter extends ArrayAdapter<Contact> {
+            ContactAdapter(ArrayList<Contact> contacts){
+                super(getActivity(), R.layout.contact_list_row, R.id.contact_row_name, contacts);
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                convertView = super.getView(position, convertView, parent);
+
+                Contact contact = getItem(position);
+
+                TextView nameTextView = (TextView) convertView.findViewById(R.id.contact_row_name);
+                nameTextView.setText(contact.getName());
+                return convertView;
+            }
+        }
+
+        public interface Contract{
+            public void selectedPosition(int position);
+        }
+
+
+}
--- app/src/main/java/com/example/suhaas/mycontacts/ContactViewActivity.java
+++ app/src/main/java/com/example/suhaas/mycontacts/ContactViewActivity.java
@@ -24,161 +24,21 @@ import java.util.ArrayList;
 
 public class ContactViewActivity extends AppCompatActivity {
 
-    public static final String EXTRA = "CVA_Contact";
-
-    private int mColor;
-    private Contact mContact;
-    private int mPosition;
-    private TextView mContactName;
-    private FieldsAdapter mAdapter;
+    public static final String EXTRA = ContactViewFragment.EXTRA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_view);
 
-        Display display = getWindowManager().getDefaultDisplay();
-        Point point = new Point();
-        display.getSize(point);
-
-        int width = point.x;
-        int height = point.y;
-
-        RelativeLayout headerSection = (RelativeLayout) findViewById(R.id.contact_view_header);
-        headerSection.setLayoutParams(new LinearLayout.LayoutParams(width, (int) (width * (9.0 / 16.0))));
-
-
-        mPosition = getIntent().getIntExtra(EXTRA, 0);
-        mContact = ContactList.getInstance().get(mPosition);
-        mContactName = (TextView) findViewById(R.id.contact_view_name);
-
-
-        Toolbar toolbar = (Toolbar)findViewById(R.id.contact_view_toolbar);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem menuItem) {
-                int id = menuItem.getItemId();
-                if (id == R.id.contact_view_edit) {
-                    Intent i = new Intent(ContactViewActivity.this, ContactEditActivity.class);
-                    i.putExtra(ContactEditActivity.EXTRA, mPosition);
-                    startActivity(i);
-                    return true;
-                }
-                return false;
-            }
-        });
-        toolbar.inflateMenu(R.menu.menu_contact_view);
-
-        ListView listView = (ListView) findViewById(R.id.contact_view_fields);
-        mAdapter = new FieldsAdapter(mContact.phoneNumbers, mContact.emails);
-        listView.setAdapter(mAdapter);
-
-
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sunset);
-        Palette palette = Palette.generate(bitmap);
-        mColor = palette.getDarkVibrantSwatch().getRgb();
-
-        updateUI();
-    }
-
-    private void updateUI(){
-        mContactName.setText(mContact.getName());
-        mAdapter.notifyDataSetChanged();
-    }
-
-    private class FieldsAdapter extends BaseAdapter{
-        ArrayList<String> emails;
-        ArrayList<String> phoneNumbers;
-        FieldsAdapter(ArrayList<String> phoneNumbers, ArrayList<String> emails){
-            this.phoneNumbers = phoneNumbers;
-            this.emails = emails;
-
-        }
-        @Override
-        public int getCount() {
-            return phoneNumbers.size() +emails.size() ;
+        if (getFragmentManager().findFragmentById(R.id.view_fragment_container) == null){
+            ContactViewFragment cvf = new ContactViewFragment();
+            cvf.setArguments(getIntent().getExtras());
+            getFragmentManager().beginTransaction()
+                    .add(R.id.view_fragment_container, cvf)
+                    .commit();
         }
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null){
-                convertView = ContactViewActivity.this.getLayoutInflater().inflate(R.layout.contact_view_field_row, parent, false);
-            }
-
-            String value = (String) getItem(position);
-
-            TextView contactValue = (TextView) convertView.findViewById(R.id.contact_view_row_value);
-            contactValue.setText(value);
-
-            ImageView iv = (ImageView) convertView.findViewById(R.id.field_icon);
-            if (isFirst(position)){
-                if (isEmail(position)){
-                    iv.setImageResource(R.drawable.ic_email);
-                }else{
-                    iv.setImageResource(R.drawable.ic_call);
-                }
-            }
-            iv.setColorFilter(mColor);
-
-            return convertView;
-        }
-
-        private boolean isFirst(int position){
-            if (position == 0 || position == phoneNumbers.size()){
-                return true;
-            }
-            return false;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            if (isEmail(position)){
-                return emails.get(position - phoneNumbers.size());
-            }else{
-                return phoneNumbers.get(position);
-            }
-        }
-
-        private boolean isEmail(int position){
-            if (position > phoneNumbers.size()-1){
-                return true;
-            }else{
-                return false;
-            }
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        updateUI();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_contact_view, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
